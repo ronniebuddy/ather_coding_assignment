509e1bd91205ea1b0149d66574a9c95cc2395de1
Rohith <rohith@payobank.in>
2023-01-11 15:43:59 +0530

    Show current date time

--- app/src/main/java/com/testprojects/athercodingassignment/MainActivity.kt
+++ app/src/main/java/com/testprojects/athercodingassignment/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.testprojects.athercodingassignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.testprojects.athercodingassignment.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivityBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityBinding =
+            ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBinding.root)
+        val calendar: Calendar = Calendar.getInstance()
+
+        // Format date time in the format "Wednesday, January 11"
+        val simpleDateFormat = SimpleDateFormat("EEEE, MMMM DD", Locale.US)
+        val dateTime = simpleDateFormat.format(calendar.time).uppercase()
+        mainActivityBinding.tvCurrentDate.text = dateTime
     }
 }
